82e021d36517634ee6aa1ed48eb76518a287ca5e
Gregui-hub <56194097+Gregui-hub@users.noreply.github.com>
2019-10-11 22:04:42 -0300

    meu primeiro commit js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+
+var nome = "Victor Gregui";
+var n1 = 5;
+var n2 = 3;
+var frase = "Japao e o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos ")
+//alert(idade + idade2);
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase.toLowerCase());
+//alert(frase.replace("Japao", "Brasil"));
